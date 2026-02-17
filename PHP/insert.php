6207766bf4ad80b76eb7cd1d7d6c1ada7e0babfb
P. Krisztián <krisztianprincz@gmail.com>
2026-02-17 19:18:57 +0100

    Update insert.php

--- PHP/insert.php
+++ PHP/insert.php
@@ -46,44 +46,55 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
 		}
     </style>
 </head>
+<body>
 
 <?php
-	$name = $_POST['client_name'];
-	$balance_string = $_POST['balance'];
-	// Delete spaces so that PHP sees "1500000" instead of "1 500 000"
-	$balance = str_replace(' ', '', $balance_string);
+	if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['client_name']) && isset($_POST['balance'])) {
+		
+		// Connecting
+		require_once 'db_connect.php';  // Imports $connection from db_connect.php
+		
+		if (!$connection) {
+            die("Kapcsolódási hiba: " . mysqli_connect_error());
+        }
+		
+		$name = $_POST['client_name'];
+		$balance_string = $_POST['balance'];
+		// Delete spaces so that PHP sees "1500000" instead of "1 500 000"
+		$balance = str_replace(' ', '', $balance_string);
 
-	// Connecting
-	require_once 'db_connect.php';  // Imports $connection from db_connect.php
-
-	if (!$connection) {
-		die("Kapcsolódási hiba: " . mysqli_connect_error());
-	}
-
-	// Prepared statement
-	$sql = "INSERT INTO accounts (client_name, balance) VALUES (?, ?)";
-	$statement = mysqli_prepare($connection, $sql);
-
-	if ($statement) {
-		// Bind parameters: "s" (string) for name, "i" (integer) for balance
-		mysqli_stmt_bind_param($statement, "si", $name, $balance);
-
-		// Execute
-		if (mysqli_stmt_execute($statement)) {
-			echo "Sikeres mentés!";		
-		} else {
-			echo "Hiba történt: " . mysqli_error($connection);
+		if (!$connection) {
+			die("Kapcsolódási hiba: " . mysqli_connect_error());
 		}
 
-		// Closing statement
-		mysqli_stmt_close($statement);
+		// Prepared statement
+		$sql = "INSERT INTO accounts (client_name, balance) VALUES (?, ?)";
+		$statement = mysqli_prepare($connection, $sql);
 
-	} else {
-		echo "Hiba az előkészítésben: " . mysqli_error($connection);
-	}
+		if ($statement) {
+			// Bind parameters: "s" (string) for name, "i" (integer) for balance
+			mysqli_stmt_bind_param($statement, "si", $name, $balance);
 
+			// Execute
+			if (mysqli_stmt_execute($statement)) {
+				echo "Sikeres mentés!";		
+			} else {
+				echo "Hiba történt: " . mysqli_error($connection);
+			}
+
+			// Closing statement
+			mysqli_stmt_close($statement);
+
+		} else {
+			echo "Hiba az előkészítésben: " . mysqli_error($connection);
+		}
+		
 	// Closing connection
 	mysqli_close($connection);
+		
+	} else {
+		echo "<h2>Nincs feldolgozandó adat.</h2>";
+	}
 ?>
 
 <br>
@@ -97,4 +108,5 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
     <div style="margin-top: 50px; text-align: right;">
         <small style="color: var(--text-muted);">&copy; Princzinger Krisztián 2026</small>
     </div>
-	
+</body>
+</html>
